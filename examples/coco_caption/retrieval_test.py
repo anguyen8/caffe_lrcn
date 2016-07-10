7be8fb363bdfb8c8dfce4e4b2793261704c6cf3b
Anh Nguyen <anh.ng8@gmail.com>
2016-07-10 11:28:14 -0600

    Updated

--- examples/coco_caption/retrieval_test.py
+++ examples/coco_caption/retrieval_test.py
@@ -11,28 +11,25 @@ import string
 import sys
 
 # seed the RNG so we evaluate on the same subset each time
-np.random.seed(seed=0)
+np.random.seed(0)
 
 # from coco_to_hdf5_data import *
 from test_captioner import Captioner
 
 def main():
-  MAX_IMAGES = 1  # -1 to use all images
   
   ITER = 110000
   MODEL_FILENAME = 'lrcn_caffenet_iter_%d' % ITER
-  DATASET_NAME = 'val'
-
   MODEL_DIR = "/raid/anh/from_jeffdonahue_lrcn"
   MODEL_FILE = '%s/%s.caffemodel' % (MODEL_DIR, MODEL_FILENAME)
-  IMAGE_NET_FILE = './models/bvlc_reference_caffenet/deploy.prototxt'
+  #IMAGE_NET_FILE = './models/bvlc_reference_caffenet/deploy.prototxt'
+  IMAGE_NET_FILE = '/home/anh/workspace/upconvnet/caffenet/encoder.prototxt'
   LSTM_NET_FILE = './examples/coco_caption/lrcn_word_to_preds.deploy.prototxt'
   VOCAB_FILE = './examples/coco_caption/h5_data/buffer_100/vocabulary.txt'
   DEVICE_ID = 0
   with open(VOCAB_FILE, 'r') as vocab_file:
     vocab = [line.strip() for line in vocab_file.readlines()]
   
-  if MAX_IMAGES < 0: MAX_IMAGES = len(dataset.keys())
   captioner = Captioner(MODEL_FILE, IMAGE_NET_FILE, LSTM_NET_FILE, VOCAB_FILE,
                         device_id=DEVICE_ID)
 
@@ -46,12 +43,13 @@ def main():
   # output_captions, _ = captioner.sample_captions( descriptor, temp=temp )
   # caption = output_captions[0]
   strategy = { "type": "sample", "temp": float("inf")}
-  caption, _ = captioner.sample_caption(descriptor, strategy=strategy, max_length=20)
+  caption, probs = captioner.sample_caption(descriptor, strategy=strategy, max_length=20)
 
-  print ">>> output_captions", caption
+#  print ">>> output_captions", caption
   
   words_caption = captioner.sentence(caption)
-  print ">>>> : [", words_caption, "]"
+  score = np.mean(probs)
+  print ">>>> : [", words_caption, "] %.2f" % score
 
 if __name__ == "__main__":
   main()
