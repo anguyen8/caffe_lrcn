774349aa191fa02916564c34971afa18255b5ac1
Anh Nguyen <anh.ng8@gmail.com>
2016-07-10 11:28:22 -0600

    Updated

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+img="${1}"
+
 ver=7.0
 export LD_LIBRARY_PATH=/home/anh/local/lib:/home/anh/160503__cudnn/cuda-${ver}/targets/x86_64-linux/lib:/home/anh/anaconda/lib:${LD_LIBRARY_PATH}
 
-python examples/coco_caption/retrieval_test.py
+rm -rf retrieval_cache/
+python examples/coco_caption/retrieval_test.py ${img}
